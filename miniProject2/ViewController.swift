405e5af043a6bf15b78e77d55032058f96279313
Quinn Farr <quinnkf@me.com>
2020-07-23 20:07:31 +0200

    Initial Commit

---
+++ miniProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miniProject2
+//
+//  Created by Quinn Farr on 23.07.20.
+//  Copyright © 2020 Quinn Farr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
